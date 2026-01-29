8e3af0dbb055b30d40167c2d3c1d3e05f111febb
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-29 11:03:40 +0530

    playwright Hover concept  code pratice

--- automation/actionsConcept.spec.ts
+++ automation/actionsConcept.spec.ts
@@ -8,4 +8,8 @@ test('Acction Conncept', async ({ page }) => {
     const hoverElement = page.locator("//button[@id='products-dd-toggle']");
     await hoverElement.hover();
 
+    const innerElementHover = page.locator("//button[@id='products-dd-tab-3']");
+    await innerElementHover.hover();  
+      
+
  });

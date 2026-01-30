6e38480d66d4016c3489bb81ba47d4ee3b1af00b
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-30 14:39:12 +0530

    playwright code pratice

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,4 +4,7 @@ test('Login Page', async ({ page }) => {
 
     await page.goto("https://www.saucedemo.com/");
 
+    const userName = page.locator("//input[@id='user-name']");
+    await userName.fill("standard_user");
+
 })

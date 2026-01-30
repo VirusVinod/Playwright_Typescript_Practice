10286f3eee9e3cb51fab08bfd6750cd48440dcdf
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-30 14:32:46 +0530

    playwright code pratice

--- tests/amzonn.spec.ts
+++ tests/amzonn.spec.ts
@@ -15,11 +15,21 @@ test('test case', async ({ page }) => {
     const actualText = (await resultsHeading.textContent())?.trim();
     await expect(actualText).toBe(expectedPage);
 
+    const expectedProduct = "iPhone 17 Pro Max 2 TB: 17.42 cm (6.9″) Display with Promotion, A19 Pro Chip, Best Battery Life in Any iPhone Ever, Pro Fusion Camera System, Center Stage Front Camera; Silver";
+    const product = page.locator("//div[@data-cy='title-recipe']//a//h2");
+    const count = await product.count();
 
+    for (let i = 0; i < count; i++) {
 
+        const products = product.nth(i);
+        const actualProduct = (await products.textContent())?.trim();
+        if (actualProduct === expectedProduct) {
+            await products.scrollIntoViewIfNeeded();
+            await products.click();
+            break;
+        }
 
-
-
+    }
 
 
 })

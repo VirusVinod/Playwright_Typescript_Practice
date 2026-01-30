3ac967251be82b2be881d660cc4bc1dbe1db4290
vinod singh <vinodkumarsingh680@gmail.com>
2026-01-30 14:20:24 +0530

    playwright code pratice

--- tests/amzonn.spec.ts
+++ tests/amzonn.spec.ts
@@ -10,5 +10,16 @@ test('test case', async ({ page }) => {
     const searchBtnClick = page.locator("//input[@id='nav-search-submit-button']");
     await searchBtnClick.click();
 
+    const expectedPage = "Results";
+    const resultsHeading = page.locator("//h2[@class='a-size-medium-plus a-spacing-none a-color-base a-text-bold']");
+    const actualText = (await resultsHeading.textContent())?.trim();
+    await expect(actualText).toBe(expectedPage);
+
+
+
+
+
+
+
 
 })

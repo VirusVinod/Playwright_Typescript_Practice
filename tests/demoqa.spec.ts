1106fd699d3f3e03498c1758b4d0161fbf4c34eb
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-01 11:25:19 +0530

    playwright code pratice

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -8,7 +8,13 @@ test('demoqa page', async ({ page }) => {
     await firstName.fill("Test");
 
     const lastName = page.locator("//input[@id='lastName']");
-    await lastName.fill("Test Test")
+    await lastName.fill("Test Test");
+
+    const email = page.locator("//input[@id='userEmail']");
+    await email.fill("Test231ert@gmail.com");
+
+    const gender = page.locator("//input[@id='gender-radio-1']");
+    await gender.click({ force: true });
 
 
 

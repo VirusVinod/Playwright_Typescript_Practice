c0f2c0ced8dd5102ea1abd4a8fb1ac4613450bba
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-01 11:53:14 +0530

    playwright code pratice

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -16,6 +16,34 @@ test('demoqa page', async ({ page }) => {
     const gender = page.locator("//input[@id='gender-radio-1']");
     await gender.click({ force: true });
 
+    const userMobileNumber = page.locator("//input[@id='userNumber']");
+    await userMobileNumber.fill("8802800000");
+
+    const dateOfBirth = page.locator("//input[@id='dateOfBirthInput']");
+    await dateOfBirth.click();
+
+    const monthDropDown = page.locator("//select[@class='react-datepicker__month-select']");
+    await monthDropDown.selectOption({ label: "October" });
+
+    const yearDropDown = page.locator("//select[@class='react-datepicker__year-select']");
+    await yearDropDown.selectOption({ label: "2011" });
+
+    const expectedDate = "8";
+    const date = page.locator("//div[@role='option']");
+    const counts = await date.count();
+
+    for (let i = 0; i < counts; i++) {
+        const actual = date.nth(i);
+        const actualDate = (await actual.textContent())?.trim();
+        if (actualDate === expectedDate) {
+            await actual.scrollIntoViewIfNeeded();
+            await actual.click();
+            console.log("Actual date:", actualDate);
+            break;
+        }
+    }
+
+
 
 
 })

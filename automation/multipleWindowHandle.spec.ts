37312e91ee4808a0e08075c978b93a95d1667c30
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-02 20:01:04 +0530

    playwright code pratice

--- automation/multipleWindowHandle.spec.ts
+++ automation/multipleWindowHandle.spec.ts
@@ -1,8 +1,11 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Multiple window handle', async ({page})=>{
+test('Multiple window handle', async ({ page }) => {
 
     await page.goto("https://www.hyrtutorials.com/p/window-handles-practice.html");
+
+    const openMultipleTabs = page.locator("#newTabsBtn");
+    await openMultipleTabs.click();
 
 
 })

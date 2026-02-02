52919f7c3ca5f7ffa193c18bfd605b2d13d0bfa5
vinod singh <vinodkumarsingh680@gmail.com>
2026-02-02 20:44:37 +0530

    playwright code pratice

--- automation/windowHandle.spec.ts
+++ automation/windowHandle.spec.ts
@@ -1,3 +1,27 @@
 import { test, expect } from '@playwright/test';
 
+test('Multiple window handle', async ({ page, context }) => {
 
+    await page.goto("https://www.hyrtutorials.com/p/window-handles-practice.html");
+
+    const parentPage = page;
+
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        page.click('#newWindowsBtn')
+    ]);
+
+    await newPage.waitForLoadState();
+    await newPage.bringToFront();
+
+    const title = await newPage.title();
+    expect(title).toContain('Basic Controls');
+
+    const fisrtName = newPage.locator("#firstName");
+    await fisrtName.fill("Vinod");
+
+    const lastName = newPage.locator("#lastName");
+    await lastName.fill("Singh");
+
+    await parentPage.bringToFront();
+});
